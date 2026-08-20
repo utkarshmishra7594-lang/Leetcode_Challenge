533efbd09af33247a4e59cbc82ea29951642f6b2
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2026-08-20 17:40:37 +0530

    feat: add Koko Eating Bananas solution

--- algorithms/koko-eating-bananas/solution.java
+++ algorithms/koko-eating-bananas/solution.java
@@ -1,37 +1,33 @@
 class Solution {
-    public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer>result=new ArrayList<>();
-        Arrays.sort(nums);
-        int n=nums.length;
-        int low=0;
-        int high=n-1;
+    public int minEatingSpeed(int[] piles, int h) {
+        int n=piles.length;
+        int low=1;
+        int high=Arrays.stream(piles).max().getAsInt();
+        int result=-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==target){
-                int left=mid;
-                while(left>=0 && nums[left]==target){
-                    result.add(left);
-                    left--;
-                }
-                int right=mid+1;
-                while(right<n && nums[right]==target){
-                    result.add(right);
-                    right++;
-                }
-                break;
-
-            }else if(nums[mid]<target){
-                low=mid+1;
-
+            int guess=(low+high)/2;
+            long hour=timetofinish(piles,n,guess);
+            if(hour>h){
+                low=guess+1;
             }else{
-                high=mid-1;
+                result=guess;
+                high=guess-1;
             }
 
+
         }
-        Collections.sort(result);
         return result;
-
-
         
+    }
+    //helper function
+    private long timetofinish(int[] piles,int n,int speed){
+        long time=0;
+        for(int i=0;i<n;i++){
+            time+=piles[i]/speed;
+            if(piles[i]%speed!=0){
+                time++;
+            }
+        }
+        return time;
     }
 }
