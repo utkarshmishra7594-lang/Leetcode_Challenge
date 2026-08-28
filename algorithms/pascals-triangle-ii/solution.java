e5bef114ff4a1dbd35fa0143cf9f8da414655e8f
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2026-08-28 21:16:20 +0530

    feat: add Pascal's Triangle II solution

--- algorithms/pascals-triangle-ii/solution.java
+++ algorithms/pascals-triangle-ii/solution.java
@@ -1,22 +1,13 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>>result=new ArrayList<>();
-        for(int row=0;row<numRows;row++){
-            List<Integer>currentRow=new ArrayList<>();
-            for(int col=0;col<=row;col++){
-                if(col==0 || col==row){
-                    currentRow.add(1);
-                }else{
-                    int val=result.get(row-1).get(col-1)+result.get(row-1).get(col);
-                    currentRow.add(val);
-                }
-                
-            }
-            result.add(currentRow);
-            
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer>result=new ArrayList<>();
+        long val=1;
+        for(int k=0;k<=rowIndex;k++){
+            result.add((int)val);
+            val=val*(rowIndex-k)/(k+1);
         }
         return result;
-
+        
         
     }
 }
