145a61b971351d943f6c9a1dab4892c935a940d5
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2026-08-29 19:15:15 +0530

    feat: add Degree of an Array solution

--- algorithms/degree-of-an-array/solution.java
+++ algorithms/degree-of-an-array/solution.java
@@ -1,13 +1,27 @@
 class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        List<Integer>result=new ArrayList<>();
-        long val=1;
-        for(int k=0;k<=rowIndex;k++){
-            result.add((int)val);
-            val=val*(rowIndex-k)/(k+1);
+    public int findShortestSubArray(int[] nums) {
+        HashMap<Integer,Integer>count=new HashMap<>();
+        HashMap<Integer,Integer>first=new HashMap<>();
+        HashMap<Integer,Integer>last=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            count.put(nums[i],count.getOrDefault(nums[i],0)+1);
+            if(!first.containsKey(nums[i])){
+                first.put(nums[i],i);
+            }
+            last.put(nums[i],i);
+
+            
         }
-        return result;
-        
+
+        int degree=Collections.max(count.values());
+        int ans=nums.length;
+        for(int x:count.keySet()){
+            if(count.get(x)==degree){
+                ans=Math.min(ans,last.get(x)-first.get(x)+1);
+            }
+        }
+        return ans;
         
     }
 }
