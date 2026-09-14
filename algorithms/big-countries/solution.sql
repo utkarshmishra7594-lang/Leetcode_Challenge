ddd5abb224fb08e83c89f52ea869944e6daad7c2
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2026-09-14 20:57:40 +0530

    feat: add Big Countries solution

--- algorithms/big-countries/solution.sql
+++ algorithms/big-countries/solution.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
-select name from Customer
-where (referee_id!=2) or referee_id is null;
+select name,population,area
+from World
+where (area>=3000000) or (population>=25000000);
