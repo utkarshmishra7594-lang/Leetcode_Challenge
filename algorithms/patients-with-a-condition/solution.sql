ea84a48f1cf867393a91c3682e31b6d37d34eee7
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2026-09-16 22:49:18 +0530

    feat: add Patients With a Condition solution

--- algorithms/patients-with-a-condition/solution.sql
+++ algorithms/patients-with-a-condition/solution.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-select name,population,area
-from World
-where (area>=3000000) or (population>=25000000);
+select patient_id,patient_name,conditions
+from Patients
+where conditions like "DIAB1%" or
+conditions like "% DIAB1%";
