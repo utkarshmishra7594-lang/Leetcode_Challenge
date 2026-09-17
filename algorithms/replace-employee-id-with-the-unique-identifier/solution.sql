4774318cd47552f01cefbcc47cb0baeb11c26dc4
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2026-09-17 17:38:31 +0530

    feat: add Replace Employee ID With The Unique Identifier solution

--- algorithms/replace-employee-id-with-the-unique-identifier/solution.sql
+++ algorithms/replace-employee-id-with-the-unique-identifier/solution.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
-select patient_id,patient_name,conditions
-from Patients
-where conditions like "DIAB1%" or
-conditions like "% DIAB1%";
+
+select euni.unique_id as unique_id,e.name as name
+from Employees as e
+left join EmployeeUNI as euni
+on e.id=euni.id;
+
+
