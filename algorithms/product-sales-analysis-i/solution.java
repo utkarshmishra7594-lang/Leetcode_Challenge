90ca8f990baddbaf6bfb0b2c1798883fc97c831c
utkarshmishra7594-lang <utkarshmishra7594@gmail.com>
2026-09-19 17:23:27 +0530

    feat: add Product Sales Analysis I solution

---
+++ algorithms/product-sales-analysis-i/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>>result=new ArrayList<>();
+        for(int row=0;row<numRows;row++){
+            List<Integer>currentRow=new ArrayList<>();
+            for(int col=0;col<=row;col++){
+                if(col==0 || col==row){
+                    currentRow.add(1);
+                }else{
+                    int val=result.get(row-1).get(col-1)+result.get(row-1).get(col);
+                    currentRow.add(val);
+                }
+                
+            }
+            result.add(currentRow);
+            
+        }
+        return result;
+
+        
+    }
+}
